91c514c3c281a1c40b4718489595dd801fde55ba
Adrien <verblodung@gmail.com>
2018-02-05 21:07:16 -0500

    adding nlogn solution using hashtable

--- twoNumberSum.js
+++ twoNumberSum.js
@@ -1,10 +1,23 @@
+// function twoNumberSum(array, targetSum) {
+//   for (let i = 0; i < array.length; i++) {
+//     if (
+//       array.includes(targetSum - array[i]) &&
+//       array[i] !== targetSum - array[i]
+//     )
+//       return [array[i], targetSum - array[i]].sort((a, b) => a - b)
+//   }
+//   return []
+// }
+
 function twoNumberSum(array, targetSum) {
-  for (let i = 0; i < array.length; i++) {
-    if (
-      array.includes(targetSum - array[i]) &&
-      array[i] !== targetSum - array[i]
-    )
-      return [array[i], targetSum - array[i]].sort((a, b) => a - b)
+  let left = 0,
+    right = array.length - 1
+  array = array.sort((a, b) => a - b)
+  while (left !== right) {
+    if (array[left] + array[right] === targetSum)
+      return [array[left], array[right]]
+    else if (array[left] + array[right] > targetSum) right--
+    else left++
   }
   return []
 }
