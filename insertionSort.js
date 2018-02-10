c326971a444b9b3998e9e02c111b024824d087ed
Adrien <verblodung@gmail.com>
2018-02-10 17:16:29 -0500

    solved

--- insertionSort.js
+++ insertionSort.js
@@ -2,15 +2,19 @@ function insertionSort(array) {
   let sorted = [array.shift()]
   while (array.length) {
     const newElement = array.shift()
+    console.log(sorted, newElement, array)
     if (newElement <= sorted[0]) {
       sorted.unshift(newElement)
     } else if (newElement >= sorted[sorted.length - 1]) {
       sorted.push(newElement)
     } else {
       for (let i = 0; i < sorted.length; i++) {
-        const low = sorted[i] || -Infinity
-        const high = sorted[i + 1] || Infinity
+        let low = sorted[i]
+        let high = sorted[i + 1]
+        if (typeof sorted[i] !== 'number') low = -Infinity
+        if (typeof sorted[i] !== 'number') high = Infinity
         if (newElement > low && newElement <= high) {
+          console.log(newElement, 'between', low, high, 'in', sorted)
           sorted.splice(i + 1, 0, newElement)
           break
         }
@@ -20,4 +24,4 @@ function insertionSort(array) {
   return sorted
 }
 
-console.log(insertionSort([8, 5, 2, 9, 10, 6, 3]))
+console.log(insertionSort([0, 0, 1, 3, 0, 5, 3, 4]))
