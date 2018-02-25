2f6a1cf787cd2f0dce13fd4d143f0931aebd3bd9
Adrien <verblodung@gmail.com>
2018-02-24 19:39:24 -0500

    breadth first done

--- breadthFirstSearch.js
+++ breadthFirstSearch.js
@@ -10,6 +10,15 @@ class Node {
   }
 
   breadthFirstSearch(array) {
-    // Write your code here.
+    const queue = [this]
+    while (queue.length) {
+      let current = queue[0]
+      array.push(current.name)
+      queue.shift()
+      for (let child of current.children) {
+        queue.push(child)
+      }
+    }
+    return array
   }
 }
