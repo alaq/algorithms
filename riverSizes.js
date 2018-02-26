90bfb152481e8310c06d9069566a5ad12eb8dc7f
Adrien Lacquemant <verblodung@gmail.com>
2018-02-26 11:31:47 -0500

    riverSizes

--- riverSizes.js
+++ riverSizes.js
@@ -1,22 +1,36 @@
 function riverSizes(matrix) {
-  const visitedMatrix = [...Array(matrix.length).keys()].map(i =>
-    Array(matrix[0].length)
-  )
+  const visited = [...Array(matrix.length).keys()].map(i => Array(matrix[0].length).fill(false))
+  let rivers = []
 
-  function followRiver(row, column) {
-      
-    if (matrix[row][column] === 1 && visitedMatrix[row][column] !== 1) {
-        if (matrix[row][column + 1] === 1)
+  function followRiver(i, j) {
+    let size = 1,
+      continues = true
+    while (continues) {
+      visited[i][j] = true
+      continues = false
+      if (matrix[i][j + 1] === 1) {
+        size++
+        j = j + 1
+        continues = true
+        continue
+      } else if (i + 1 <= matrix.length - 1 && matrix[i + 1][j] === 1) {
+        size++
+        i = i + 1
+        continues = true
+        continue
+      }
     }
+    return size
   }
 
   for (let i = 0; i < matrix.length; i++) {
     for (let j = 0; j < matrix[0].length; j++) {
-      followRiver(i, j)
+      if (matrix[i][j] === 1 && !visited[i][j]) {
+        rivers.push(followRiver(i, j))
+      }
     }
   }
-
-  return visitedMatrix
+  return rivers
 }
 
-console.log(riverSizes([[0, 1], [1, 0]]))
+console.log(riverSizes([[0, 1, 1], [1, 0, 1]]))
