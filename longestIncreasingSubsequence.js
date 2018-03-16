93959f1a5551e2affcbe5dec1aa1b08fd87d5bf8
Adrien Lacquemant <verblodung@gmail.com>
2018-03-16 14:31:45 -0400

    longest increasing subsequence: dynamic programming

--- longestIncreasingSubsequence.js
+++ longestIncreasingSubsequence.js
@@ -1,3 +1,5 @@
+const seq = [1, 2, 5, 4]
+
 // Brute force, recursive way
 function longestIncreasingSubsequence(sequence, index = 0, base = -Infinity) {
     if (index === sequence.length) return 0
@@ -7,8 +9,23 @@ function longestIncreasingSubsequence(sequence, index = 0, base = -Infinity) {
     if (base >= num) return excluding
 
     const including = 1 + longestIncreasingSubsequence(sequence, index + 1, num)
-    
+
     return Math.max(excluding, including)
 }
 
-console.log(longestIncreasingSubsequence([1, 2, 5, 4])) // should return 3
+console.log(longestIncreasingSubsequence(seq)) // should return 3
+
+// Dynamic programming
+function lisDynamic(sequence) {
+    const length = new Array(sequence.length).fill(null)
+    for (let i = 0; i < sequence.length; i++) {
+        let max = 1
+        for (let j = 0; j < i; j++) {
+            if (sequence[i] > sequence[j] && length[j] + 1 > max) max = length[j] + 1
+        }
+        length[i] = max
+    }
+    return Math.max(...length)
+}
+
+console.log(lisDynamic(seq)) // should return 3
