8836a3ffbe151435e99b8daf18773133c5159c14
Adrien Lacquemant <verblodung@gmail.com>
2018-03-27 16:53:39 -0400

    correct triangle solution

--- triangles.js
+++ triangles.js
@@ -4,16 +4,30 @@
 // There are three triangles possible {3, 4, 6}, {4, 6, 7} and {3, 6, 7}. 
 // Note that {3, 4, 7} is not a possible triangle.
 
-function triangles(inputArray) {
-    // lets test if 3 4 6 is a triangle
-    return testTriangle([3,4,6])
+function triangles(array) {
+    let power = [[]]
+    let validTriangles = []
+    for (const elem of array) {
+        const tempPower = []
+        for (let i = 0; i < power.length; i++) {
+            let potentialTriangle = power[i].concat([elem])
+            if (potentialTriangle.length === 3 && testTriangle(potentialTriangle)) {
+                validTriangles.push(potentialTriangle)
+            }
+            tempPower.push(potentialTriangle)
+        }
+        power = power.concat(tempPower)
+    }
+    return validTriangles
 }
 
 function testTriangle(sides) {
-    const max = Math.max(...sides)
-    sides.splice(sides.indexOf(max), 1)
-    if (max - sides[0] - sides[1] < 0) return true
+    sidesCopy = sides.slice(0)
+    const max = Math.max(...sidesCopy)
+    sidesCopy.splice(sides.indexOf(max), 1)
+    if (max - sidesCopy[0] - sidesCopy[1] < 0) return true
     else return false
 }
 
 console.log(triangles([4, 6, 3, 7]))
+// console.log(triangles([4, 6]))
