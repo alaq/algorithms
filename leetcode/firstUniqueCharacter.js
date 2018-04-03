bbb2c71df0c2609389b12de7ef9d41af5b10ae64
Adrien Lacquemant <verblodung@gmail.com>
2018-04-02 22:43:07 -0400

    first unique character solved

--- leetcode/firstUniqueCharacter.js
+++ leetcode/firstUniqueCharacter.js
@@ -6,10 +6,15 @@
  * @return {number}
  */
 var firstUniqChar = function(s) {
+    let letters = []
     for (let i = 0; i < s.length; i++) {
-        if (s.slice(i).indexOf(s[i]) === -1) return i
+        if (s.slice(i+1) === 0) return i
+        else if (s.slice(i + 1).indexOf(s[i]) === -1 && !letters.includes(s[i])) return i
+        else letters.push(s[i])
     }
     return -1
 };
 
 console.log(firstUniqChar('loveleetcode'))
+console.log(firstUniqChar('cc'))
+console.log(firstUniqChar('aadadaad'))
