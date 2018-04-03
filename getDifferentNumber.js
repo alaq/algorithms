ef6f4e66a0b88439bc6c78f0643f0136da75e8e4
Adrien <verblodung@gmail.com>
2018-04-03 00:25:22 -0400

    get different number in place

--- getDifferentNumber.js
+++ getDifferentNumber.js
@@ -6,9 +6,34 @@ function getDifferentNumber(arr) {
     }
 }
 
-function getDiffentNumberSet(arr) {
+// Set method
+function getDifferentNumberSet(arr) {
     const set = new Set(arr)
-    return set
+    for (let i = 0; i < arr.length; i++) {
+        if (!set.has(i)) return i
+    }
+    return arr.length
 }
 
-console.log(getDifferentNumberSet([5,2,4,1]))
+console.log(getDifferentNumberSet([5,2,4,1,0]))
+
+
+// in place
+function getDifferentNumberInPlace(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        while (arr[i] < arr.length && arr[arr[i]] !== arr[i]) arr = swap(arr, i)
+    }
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== i) return i
+    }
+    return arr.length + 1
+}
+
+function swap(arr, i) {
+    temp = arr[arr[i]]
+    arr[arr[i]] = arr[i]
+    arr[i] = temp
+    return arr
+}
+
+console.log(getDifferentNumberInPlace([5,2,4,1,0]))
