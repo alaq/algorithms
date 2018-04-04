81bb4c096cf238f40b5831ca976306d79a5ce2f6
Adrien Lacquemant <verblodung@gmail.com>
2018-04-04 19:07:13 -0400

    string compression wip

---
+++ stringCompression.js
@@ -0,0 +1,17 @@
+/**
+ * @param {character[]} chars
+ * @return {number}
+ */
+
+var compress = function(chars) {
+    let i = 1
+    let current = chars[0]
+    let count = 0
+    while (i < chars.length) {
+        current = chars[i]
+        while (chars[i + c + 1] === current) {
+            c++
+            cha 
+        }
+    }
+};
