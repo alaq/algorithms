6e53ddec04e66ae4ef6f691d33b19ecc1cdbb169
Adrien Lacquemant <verblodung@gmail.com>
2018-04-04 13:03:14 -0400

    valid parentheses

---
+++ validParentheses.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+
+var isValid = function(s) {
+    const brackets = {
+        '[' : ']',
+        '(' : ')',
+        '{' : '}',
+    }
+    let stack = []
+    for (let i = 0; i < s.length; i++) {
+        if(brackets.hasOwnProperty(s[i])) {
+            stack.push(s[i])
+        } else if (brackets[stack[stack.length - 1]] !== s[i]) return false
+        else if (brackets[stack[stack.length - 1]] === s[i]) {
+            stack.pop()
+        }
+    }
+    return stack.length === 0
+};
+
+
+console.log(isValid('([]'))
