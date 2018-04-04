9f5417bc653be3e5b693aff785c736bbddd9d29b
Adrien <verblodung@gmail.com>
2018-04-04 19:31:20 -0400

    finish string compression

--- stringCompression.js
+++ stringCompression.js
@@ -4,14 +4,21 @@
  */
 
 var compress = function(chars) {
-    let i = 1
+    let i = 0
     let current = chars[0]
     let count = 0
     while (i < chars.length) {
         current = chars[i]
+        let c = 0
         while (chars[i + c + 1] === current) {
             c++
-            cha 
         }
+        if (c > 0) {
+            chars.splice(i+1, c, c + 1)
+        }
+        i = i + c + 1
     }
+    return chars.filter(char => char)
 };
+
+console.log(compress(['a', 'a', 'b', 'b', 'b']))
