692897eda4274a2275ed4e1416d86899111992d0
Adrien Lacquemant <verblodung@gmail.com>
2018-04-06 14:35:50 -0400

    palindrome linked list

--- palindromeLinkedList.js
+++ palindromeLinkedList.js
@@ -9,14 +9,19 @@
  * @param {ListNode} head
  * @return {boolean}
  */
+
 var isPalindrome = function(head) {
+    if(!head) return true
     let p1 = head
     while (p1.next) {
+        p1.next.previous = p1
         p1 = p1.next
     }
     let p2 = head
     while (p2.next) {
         if (p1.val !== p2.val) return false
+        p1 = p1.previous
+        p2 = p2.next
     }
     return true
 };
