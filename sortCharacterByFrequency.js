2f790a37afc2220e8b1ac1d8baae5d56d42ead1c
Adrien Lacquemant <verblodung@gmail.com>
2018-04-08 17:16:03 -0400

    sort charactert by frequency

--- sortCharacterByFrequency.js
+++ sortCharacterByFrequency.js
@@ -8,5 +8,20 @@ var frequencySort = function(s) {
         if (freq.hasOwnProperty(s[i])) freq[s[i]]++
         else freq[s[i]] = 1
     }
-    Object.entries(freq).sort((a, b))
+    const freqObj = Object.keys(freq)
+        .sort((a, b) => freq[b] - freq[a])
+        .reduce((a, v) => {
+            a[v] = freq[v]
+            return a; 
+        },  {})
+    let fString = ''
+    for (letter in freqObj) {
+        for (let i = 0; i < freqObj[letter]; i++) {
+            fString += letter
+        }
+    }
+    return fString
 };
+
+console.log(frequencySort('adrienaaaaghei'))
+console.log(frequencySort("2a554442f544asfasssffffasss"))
