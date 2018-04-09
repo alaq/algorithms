8a40dfcb176602bd1cf8442797443361db9913ee
Adrien Lacquemant <verblodung@gmail.com>
2018-04-08 21:53:32 -0400

    unique paths ii completed

--- uniquePaths-ii.js
+++ uniquePaths-ii.js
@@ -7,13 +7,11 @@ var uniquePathsWithObstacles = function(obstacleGrid) {
     let paths = new Array(obstacleGrid.length).fill(null).map(() => new Array(obstacleGrid[0].length).fill(0))
     for (let i = 0; i < paths.length; i++) {
         for (let j = 0; j < paths[i].length; j++) {
-            let top = i - 1 < 0 || j - 1 < 0 ? 0 : paths[i-1][j]
-            let left = i - 1 < 0 || j - 1 < 0 ? 0 : paths[i][j-1]
-            console.log(top, left)
+            let top = i - 1 < 0 ? 0 : paths[i-1][j]
+            let left = j - 1 < 0 ? 0 : paths[i][j-1]
             if (i === 0 && j === 0 && obstacleGrid[0][0] !== 1) paths[0][0] = 1
             else if (obstacleGrid[i][j] === 1) paths[i][j] = 0
             else paths[i][j] = top + left
-            console.log(paths, top, left)
         }
     }
     return paths[paths.length - 1][paths[0].length - 1]
