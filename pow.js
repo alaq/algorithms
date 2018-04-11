df5958851b1f08dffb0bf663eb0a1f4950e3ced6
Adrien <verblodung@gmail.com>
2018-04-10 22:27:39 -0400

    better pow function

--- pow.js
+++ pow.js
@@ -23,4 +23,12 @@ var myPowOld = function(x, n) {
     return result
 };
 
-console.log(myPow(5,2))
+
+var myPow = function(x, n) {
+    if (n === 0) return 1
+    if (n < 0) return 1 / myPow(x, -n)
+    if (n % 2) return x * myPow(x, n-1)
+    return myPow(x*x, n/2)
+}
+
+console.log(myPow(5,10))
